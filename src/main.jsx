0b3fdb16f74be652a0d415c428d2777cc4f045e9
f-fiaux <97934893+f-fiaux@users.noreply.github.com>
2026-02-28 16:31:51 -0800

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ReliefApp from './relief-signup.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ReliefApp />
+  </React.StrictMode>,
+)
